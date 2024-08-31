e54c529e99e24ce4600dbda146b27c3fe723a485
Croos3r <dorian.moy@epitech.eu>
2024-09-01 07:09:58 +0900

    feat(pokemon-db-entity): implementation of Pokemon database entity
    Took 27 seconds

---
+++ src/entities/Pokemon.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity()
+export default class Pokemon {
+	@PrimaryGeneratedColumn("rowid")
+	id!: number;
+
+	@Column("smallint", { unsigned: true })
+	pokeapiId!: number;
+
+	@Column("smallint", { unsigned: true, default: 1 })
+	level!: number;
+
+	@Column("bigint", { unsigned: true, default: 0 })
+	experience!: bigint;
+
+	@Column("varchar", { nullable: true, default: null })
+	storedBy!: string | null;
+
+	@Column("varchar", { nullable: true, default: null })
+	heldBy!: string | null;
+}
