fcecc3e0cccb11b3919d3fae064872124cc3d131
Croos3r <dorian.moy@epitech.eu>
2024-09-01 07:09:31 +0900

    feat: proper env variables typing
    Took 19 seconds

---
+++ src/env.d.ts
@@ -0,0 +1,16 @@
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			BOT_TOKEN: string;
+			REDIS_HOST: string;
+			REDIS_PORT: number;
+			DB_HOST: string;
+			DB_PORT: number;
+			DB_USER: string;
+			DB_PASS: string;
+			DB_NAME: string;
+		}
+	}
+}
+
+export type {}
