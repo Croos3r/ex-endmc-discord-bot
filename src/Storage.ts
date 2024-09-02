6c04444d675141c392b4178967da785c0f7148ac
Croos3r <dorian.moy@epitech.eu>
2024-09-02 14:13:46 +0900

    feat(storage): implemented pokemon individual view
    Took 1 hour 5 minutes

--- src/Storage.ts
+++ src/Storage.ts
@@ -11,6 +11,7 @@ import {
 } from 'discord.js'
 import { ButtonComponent, Discord, Slash, SlashGroup, SlashOption } from 'discordx'
 import { getCachedByIdOrCacheResult, invalidateCache } from './Cache.js'
+import { CONFIGURATION } from './Configuration.js'
 import DATA_SOURCE from './Database.js'
 import { getPokemonDetails } from './PokeAPI.js'
 import Pokemon from './entities/Pokemon.js'
@@ -108,11 +109,58 @@ export class Storage {
 
 	static async getPCPagePokemons(targetUser: User, pageNumber: number) {
 		const pokemons = DATA_SOURCE.getRepository(Pokemon)
-		const targetUsersPokemons = await pokemons.find({where: {storedBy: targetUser.id}, skip: pageNumber ? (pageNumber - 1) * 24 : 0, take: 24})
+		const targetUsersPokemons = await pokemons.find({where: {storedBy: targetUser.id}, skip: pageNumber ? (pageNumber - 1) * 24 : 0, take: 24, order: {id: 'asc'}})
 
 		if (targetUsersPokemons.length === 0) return []
 
 		return await Promise.all(targetUsersPokemons.map(Storage.getDatabasePokemonDetails))
+	}
+
+	static async getPokemonViewComponents(targetUser: User, pokemonId: number) {
+		const pokemons = DATA_SOURCE.getRepository(Pokemon)
+		const pokemon = await pokemons.findOne({where: [{id: pokemonId, storedBy: targetUser.id}, {id: pokemonId, heldBy: targetUser.id}]
+	})
+		if (!pokemon) return {content: `Pokemon #${pokemonId} not found in **${targetUser.displayName}**'s PC`, ephemeral: true}
+
+		const pokemonDetails = await getCachedByIdOrCacheResult(`pokemon:${pokemon.pokeAPIId}`, () => getPokemonDetails(pokemon.pokeAPIId.toString()))
+			.catch(error => {
+				if (error.response?.status !== 404) {
+					console.error(`An error occured during fetch of pokemon's details ${pokemon.id} (#${pokemon.pokeAPIId})`, error)
+					return 'error' as const
+				}
+				return 'unknown' as const
+			})
+
+		if (pokemonDetails === 'unknown' || pokemonDetails === 'error') return {content: `Pokemon #${pokemon.pokeAPIId} not found`}
+		const {level, health, attack, defense, specialAttack, specialDefense, speed} = pokemon
+		const stats = [health, attack, defense, specialAttack, specialDefense, speed]
+		return {
+			content: "",
+			embeds: [
+				new EmbedBuilder()
+					.setTitle(`${pokemonDetails.name} (No. ${pokemon.id}/#${pokemon.pokeAPIId})`)
+					// biome-ignore lint/security/noGlobalEval: only way to let the configuration have a formula
+					.setDescription(`Level: ${pokemon.level} (${pokemon.experience}/${eval(CONFIGURATION.leveling.experiencePerLevel)})
+					${pokemonDetails.stats.map(({name}, index) => `**${name}** (${stats[index]})\n\`${"#".repeat(stats[index]/255*30)}${"-".repeat(30-stats[index]/255*30)}\``).join("\n")}`)
+			],
+			components: [
+				new ActionRowBuilder<MessageActionRowComponentBuilder>({
+					components: [
+						new ButtonBuilder().setLabel('Refresh').setEmoji('🔃').setStyle(ButtonStyle.Secondary).setCustomId(`pokemon-view-${pokemon.id}-${targetUser.id}`),
+					],
+				}),
+			],
+		}
+	}
+
+	@ButtonComponent({id: /pokemon-view-\d+-\d+/})
+	async viewPokemon(interaction: ButtonInteraction) {
+		const [pokemonId, userId] = interaction.customId.split('-').slice(2)
+		const targetUser = await interaction.client.users.fetch(userId)
+		if (!targetUser) return await interaction.reply({content: 'User not found', ephemeral: true})
+		const pokemonViewComponents = await Storage.getPokemonViewComponents(targetUser, Number.parseInt(pokemonId))
+
+		await interaction.update(pokemonViewComponents)
 	}
 
 	@ButtonComponent({id: /pc-page-\d+-\d+/})
@@ -125,33 +173,41 @@ export class Storage {
 		await interaction.update(pcPageComponents)
 	}
 
-	@Slash({ description: 'View an user\'s PC stored Pokemons' })
+	@Slash({ description: 'View an user\'s PC stored specific pokemon or list of pokemons' })
 	async view(
 		@SlashOption({name: 'user', description: 'User to view', required: false, type: ApplicationCommandOptionType.User})
-		user: User | null,
+		user: User | undefined,
 		@SlashOption({name: 'page', description: 'Page number', required: false, type: ApplicationCommandOptionType.Integer})
-		pageNumber: number | null,
+		pageNumber: number | undefined,
+		@SlashOption({name: 'pokemon-id', description: 'ID of the Pokemon to view', required: false, type: ApplicationCommandOptionType.Integer})
+		pokemonId: number | undefined,
 		interaction: CommandInteraction) {
 		const targetUser = user ?? interaction.user
-		const pcPageComponents = await Storage.getPCPageComponents(targetUser, pageNumber ?? 1)
 
-		await interaction.reply({ephemeral: true, ...pcPageComponents})
+		if (pokemonId === undefined) {
+			const pcPageComponents = await Storage.getPCPageComponents(targetUser, pageNumber ?? 1)
+
+			return await interaction.reply({ephemeral: true, ...pcPageComponents})
+		}
+		console.log(JSON.stringify(pokemonId))
+		const pokemonViewCmponents = await Storage.getPokemonViewComponents(targetUser, pokemonId)
+
+		return await interaction.reply({ephemeral: true, ...pokemonViewCmponents})
 	}
-
 
 	@Slash({ description: 'Add a pokemon to an user\'s PC' })
 	async add(
 		@SlashOption({name: 'pokemon-name-or-id', description: 'Pokemon\'s name or id', required: true, type: ApplicationCommandOptionType.String})
 		pokemonNameOrId: string,
 		@SlashOption({name: 'user', description: 'User to add the pokemon', required: false, type: ApplicationCommandOptionType.User})
-		user: User | null,
+		user: User | undefined,
 		interaction: CommandInteraction,
 	) {
 		const targetUser = user ?? interaction.user
 		const lowerCaseURLEncodedPokemonName = encodeURIComponent(pokemonNameOrId.toLowerCase())
 		const pokemonDetails = await getCachedByIdOrCacheResult(`pokemon:${lowerCaseURLEncodedPokemonName}`, () => getPokemonDetails(lowerCaseURLEncodedPokemonName)).catch(error => {
 			if (error.response?.status !== 404) {
-				console.error(`An error occured during fetch of to-be-stored pokemon ${pokemon.id} (#${pokemon.pokeAPIId})`, error)
+				console.error(`An error occured during fetch of to-be-stored pokemon ${pokemon.id}(#${pokemon.pokeAPIId})`, error)
 				return 'error' as const
 			}
 			return 'unknown' as const
@@ -182,7 +238,7 @@ export class Storage {
 		@SlashOption({name: 'pokemon-id', description: 'Pokemon\'s PC id', required: true, type: ApplicationCommandOptionType.Integer})
 		pokemonPCId: number,
 		@SlashOption({name: 'user', description: 'User to remove the pokemon', required: false, type: ApplicationCommandOptionType.User})
-		user: User | null,
+		user: User | undefined,
 		interaction: CommandInteraction,
 	) {
 		const targetUser = user ?? interaction.user
